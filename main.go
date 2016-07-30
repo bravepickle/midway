b40679ac4390b3399e4cf3454883fad09f33c68d
Victor K <konovalovxxx@gmail.com>
2016-07-30 19:41:14 +0300

    save partial changes

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	//	"path/filepath"
+
+	"github.com/bravepickle/midway/stubman"
+	"github.com/urfave/negroni"
 )
 
 const defaultConfigPath = `./config.yaml`
 const argCfgInit = `config:init`
+const prefixPathStubman = `/stubman`
 
 var optHelp bool
 var cfgPath string
@@ -39,16 +44,45 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
+	n := Gateway() // Includes some default middlewares
+
+	initStubman(mux, n)
+
+	//	return
+
+	// handle the rest of URIs
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		ProxyRequest(w, req)
 	})
 
-	n := Gateway() // Includes some default middlewares
 	n.UseHandler(mux)
 
 	if Debug {
-		fmt.Printf("Listening to: \"%s\"\n", Config.App.String())
+		fmt.Printf("Listening to: %s\n", Config.App.String())
 	}
 
 	http.ListenAndServe(Config.App.String(), n)
+}
+
+func initStubman(mux *http.ServeMux, n *negroni.Negroni) {
+	// init Stubman
+	stubman.AddStubmanCrudHandlers(prefixPathStubman, mux)
+	// forward all static files to directory
+
+	//	dir := http.Dir(`/var/www/golang/src/github.com/bravepickle/midway` + prefixPathStubman + string(filepath.Separator) + stubman.StaticPath)
+	//	dir := http.Dir(prefixPathStubman + string(filepath.Separator) + stubman.StaticPath)
+	//	dir := http.Dir(prefixPathStubman + string(filepath.Separator) + stubman.StaticPath)
+	//	dir := http.Dir(stubman.StaticPath)
+	dir := http.Dir(``)
+	//	fmt.Println(`Directory `, dir)
+	//	return
+
+	//	n.Use(negroni.NewStatic(http.Dir(fmt.Sprintf(`%s/%s`, prefixPathStubman, stubman.StaticPath))))
+	handler := negroni.NewStatic(dir)
+	//	handler.Prefix = prefixPathStubman
+	n.Use(handler)
+
+	if Debug {
+		fmt.Printf("Stubman path: http://%s%s/\n", Config.App.String(), prefixPathStubman)
+	}
 }
